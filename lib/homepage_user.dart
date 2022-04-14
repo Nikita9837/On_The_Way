3caeb75bacbfe0ce87ff13fa6a093b5747171740
Aditi Sawant <90927378+Sparkle000@users.noreply.github.com>
2022-04-14 21:08:24 +0530

    userProfileStatus structure

---
+++ lib/homepage_user.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class userHomepage extends StatefulWidget {
+  const userHomepage({Key? key}) : super(key: key);
+
+  @override
+  _userHomepageState createState() => _userHomepageState();
+}
+
+class _userHomepageState extends State<userHomepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
